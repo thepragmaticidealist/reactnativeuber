9243a1bf778f8cf89d6b7095edf5c6be5798c7e2
emabishi <frankiemabishi@gmail.com>
2022-10-18 19:32:41 +0300

    Adds Homescreen page

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'; // provides store to app
+
+
+import HomeScreen from './screens/HomeScreen';
 import store from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Open up App.js to start working!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <HomeScreen />
     </Provider>
   );
 }
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -17,15 +17,16 @@ const navSlice = createSlice({
     }
   },
 })
-
+// Export actions that components will use to call functions in reducer
 export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions
 
 
-// Selectors
+// Export Selectors that components will use to fetch information from the nav slice
 export const selectOrigin = state => state.nav.origin
 export const selectDestination = state => state.nav.destination
 export const selectTravelTimeInformation = state => state.nav.travelTimeInformation
 
 // component -> action -> reducer -> slice1 in store -> slice1a in store
 
+// Export reducer so we can register it with the store
 export default navSlice.reducer
