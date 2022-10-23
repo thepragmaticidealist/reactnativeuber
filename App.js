65932c690357d5c04eacda96765ac752b8a86767
emabishi <frankiemabishi@gmail.com>
2022-10-23 20:37:46 +0300

    Adds nav favourites components

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { TailwindProvider } from 'tailwindcss-react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -18,10 +19,12 @@ export default function App() {
       <Provider store={store}>
         <TailwindProvider>
           <SafeAreaProvider>
-            <Stack.Navigator>
-              <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-              <Stack.Screen name="Map" component={MapScreen} options={{ headerShown: false }} />
-            </Stack.Navigator>
+            <KeyboardAvoidingView className="flex-1" behavior={Platform.OS === 'ios' ? 'padding': 'height' }>
+              <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Map" component={MapScreen} options={{ headerShown: false }} />
+              </Stack.Navigator>
+            </KeyboardAvoidingView>
           </SafeAreaProvider>
         </TailwindProvider>
       </Provider>
