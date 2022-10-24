3ba91bcf0f2ab37a8870b729ef33659a5617cd0b
emabishi <frankiemabishi@gmail.com>
2022-10-24 13:40:25 +0300

    Adds menu button to go home

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,27 @@
-import { View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Map from '../components/Map'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements';
+
+import { useNavigation } from '@react-navigation/native';
+
 
 
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator();
+    const navigator = useNavigation()
 
     return (
         <View>
+          <TouchableOpacity onPress={() => navigator.navigate("Home")}className="absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg">
+            <Icon name="menu" />
+          </TouchableOpacity>
+
             <View className="h-1/2">
               <Map />
             </View>
